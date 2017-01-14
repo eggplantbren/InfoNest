8a0c0c67e64b4194f2d9d092d392f0002f063fdc
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-15 08:21:14 +1300

    Implemented normal example

--- cpp/Examples/Normal.cpp
+++ cpp/Examples/Normal.cpp
@@ -3,11 +3,46 @@
 namespace InfoNest
 {
 
+Normal::Normal()
+{
+
+}
 
 
+void Normal::generate(RNG& rng)
+{
+    n1 = rng.randn();
+    n2 = rng.randn();
+    assemble();
+}
 
+double Normal::perturb(RNG& rng)
+{
+    double logH = 0.0;
 
+    // Choose a coordinate to perturb
+    int which = rng.rand_int(2);
+    double& n = (which == 0)?(n1):(n2);
 
+    logH -= -0.5*n*n;
+    n += rng.randh();
+    logH += -0.5*n*n;
+
+    assemble();
+
+    return logH;
+}
+
+void Normal::assemble()
+{
+    x = n1;
+    y = x + n2;
+}
+
+void Normal::print(std::ostream& out)
+{
+    out<<x<<' '<<y<<' ';
+}
 
 } // namespace InfoNest
 
--- cpp/Examples/Normal.h
+++ cpp/Examples/Normal.h
@@ -1,6 +1,8 @@
 #ifndef InfoNest_Normal
 #define InfoNest_Normal
 
+// Includes
+#include <ostream>
 #include "../RNG.h"
 
 namespace InfoNest
@@ -8,10 +10,16 @@ namespace InfoNest
 
 /*
 * A 2D gaussian example.
+* p(x)     ~ N(0,1)
+* p(y | x) ~ N(x,1)
+* ==>
+* p(y)     ~ N(0, sd=sqrt(2))
+* H[p(y)] = 1.76551.
 */
 class Normal
 {
     private:
+        // The two actual quantities
         double x;
         double y;
 
@@ -20,11 +28,17 @@ class Normal
         void assemble();
 
     public:
+        // Do-nothing constructor
         Normal();
 
+        // Generate from the distribution
         void generate(RNG& rng);
+
+        // Metropolis proposal
         double perturb(RNG& rng);
 
+        // Printing
+        void print(std::ostream& out);
 };
 
 } // namespace InfoNest
