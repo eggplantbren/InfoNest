23d2a346ea75d90b98ee0da66863c335779ee7c0
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-14 22:39:25 +1300

    Much more informative output

--- prototype/postprocess.py
+++ prototype/postprocess.py
@@ -2,6 +2,7 @@ __all__ = ["postprocess"]
 
 # Imports
 import numpy as np
+import matplotlib.pyplot as plt
 
 def postprocess(num_particles=1, tol=1E-3):
     """
@@ -12,19 +13,32 @@ def postprocess(num_particles=1, tol=1E-3):
     output = np.loadtxt("output.txt")
     num_particles = np.loadtxt("num_particles.txt")
 
-    # Count the number of reps
-    reps = np.sum(output[:,0] == 1)
+    # Find the starts of the runs
+    start_indices = np.nonzero(output[:,0] == 1)[0]
 
-    # Mean count above `tol`
-    mean_count = np.sum(output[:,1] > tol) / reps
-    mean_depth = mean_count / num_particles
+    reps = len(start_indices)
+    counts = np.empty(reps)
+    for i in range(0, reps):
+        start_index = start_indices[i]
+        if i < reps - 1:
+            run = output[start_index : start_indices[i+1], :]
+        else:
+            run = output[start_index : , :]
+        print("Processed run {k}/{N}.".format(k=i+1, N=reps))
+        counts[i] = (run[:,1] > tol).sum()
+
+    print("mean(counts) = {mc}.".format(mc=counts.mean()))
+    print("std (counts) = {sd}.".format(sd=counts.std()))
 
     # Estimate of the differential entropy
     # log(probability) ~= log(2 * tol * density)
-    H = mean_depth + np.log(2 * tol)
+    H = counts.mean() / num_particles + np.log(2 * tol)
+    stderr = (counts.std() / num_particles) / np.sqrt(reps)
 
-    print("Found {r} reps.".format(r = int(reps)))
-    print("H = {H} nats".format(H=H))
+    print("H = {H} +- {stderr} nats.".format(H=H, stderr=stderr))
+
+#    plt.hist(mean_depth, 100, color=[0.2, 0.2, 0.2])
+#    plt.show()
 
 if __name__ == "__main__":
     postprocess()
