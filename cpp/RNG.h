b0c06a73fe6b6cc0a61199a0b259f1ae34ebf1d1
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-15 00:24:05 +1300

    Copied DNest4's RNG class

---
+++ cpp/RNG.h
@@ -0,0 +1,53 @@
+#ifndef InfoNest_RNG
+#define InfoNest_RNG
+
+#include <random>
+
+namespace InfoNest
+{
+
+/*
+* An object of this class represents a random number generator 
+*/
+class RNG
+{
+    private:
+        // 64-bit Mersenne Twister
+        std::mt19937_64 twister;
+
+        // For uniform distribution
+        std::uniform_real_distribution<double> uniform;
+
+        // For normal distribution
+        std::normal_distribution<double> normal;
+
+    public:
+        // Constructor
+        RNG();
+
+        // Constructor that also sets seed
+        RNG(unsigned int seed);
+
+        // Set the seed (obviously)
+        void set_seed(unsigned int seed);
+
+        // Uniform(0, 1)
+        double rand();
+
+        // Normal(0, 1)
+        double randn();
+
+        // t(location=0, scale=1, nu=2)
+        double randt2();
+
+        // My favourite heavy-tailed distribution
+        double randh();
+
+        // Integer from {0, 1, 2, ..., N-1}
+        int rand_int(int N);
+}; // class RNG
+
+} // namespace InfoNest
+
+#endif
+
