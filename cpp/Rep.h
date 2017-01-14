7823e4351d304b71c83c58080b44a1622fff4eba
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-15 00:46:48 +1300

    Implemented initialise and compute_distances

--- cpp/Rep.h
+++ cpp/Rep.h
@@ -1,7 +1,10 @@
 #ifndef InfoNest_Rep
 #define InfoNest_Rep
 
+// Includes
 #include <stdlib.h>
+#include <vector>
+#include "RNG.h"
 
 namespace InfoNest
 {
@@ -20,12 +23,27 @@ class Rep
         const size_t mcmc_steps;
         const double depth;
 
+        // The reference particle
+        Particle reference_particle;
+
+        // The other particles
+        std::vector<Particle> particles;
+
+        // Distances
+        std::vector<double> distances;
+
+        // Iteration counter
+        unsigned int iteration;
+
+        // Member function to compute all the distances
+        void compute_distances();
+
     public:        
         // Constructor. You need to pass in the NS parameters
         Rep(size_t num_particles, size_t mcmc_steps, double depth);
 
-
-
+        // Initialise all the particles
+        void initialise(RNG& rng);
 
 };
 
@@ -39,16 +57,38 @@ Rep<Particle>::Rep(size_t num_particles, size_t mcmc_steps, double depth)
 :num_particles(num_particles)
 ,mcmc_steps(mcmc_steps)
 ,depth(depth)
+,particles(num_particles)
+,distances(num_particles)
+,iteration(0)
 {
 
 }
 
 
+template<class Particle>
+void Rep<Particle>::initialise(RNG& rng)
+{
+    // Generate the reference particle from the distribution
+    reference_particle.generate(rng);
+
+    // Generate the other particles
+    for(auto& p: particles)
+        p.generate(rng);
+
+    // Compute all the distances
+    compute_distances();
+
+    // Set iteration number
+    iteration = 1;
+}
 
 
-
-
-
+template<class Particle>
+void Rep<Particle>::compute_distances()
+{
+    for(size_t i=0; i<num_particles; ++i)
+        distances[i] = Particle::distance(reference_particle, particles[i]);
+}
 
 } // namespace InfoNest
 
