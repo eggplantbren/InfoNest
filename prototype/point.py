60820d6527fb3f0eaa11189c5f47bea8fbf247a4
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-14 18:04:23 +1300

    Starting

---
+++ prototype/point.py
@@ -0,0 +1,38 @@
+# Stuff accessible from outside (?)
+__all__ = ["Point"]
+
+# Imports
+import numpy as np
+import numpy.random as rng
+from utils import randh, wrap
+
+class Point:
+    """
+    An object of this class represents a point in the relevant
+    hypothesis space.
+    """
+
+    def __init__(self):
+        """
+        Constructor does nothing.
+        """
+        pass
+
+
+    def generate(self):
+        """
+        Generate the point from its probability distribution.
+        """
+        self.x = -10.0 + 20.0 * rng.rand()
+
+
+    def perturb(self):
+        """
+        Metropolis proposal that would explore the probability distribution.
+        Return ln(a) such that acceptance probability alpha = min(1, a).
+        """
+        self.x += randh()
+        self.x = wrap(self.x, -10.0, 10.0)
+        return 0.0
+
+
