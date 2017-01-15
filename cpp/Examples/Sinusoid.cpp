c9d6655f4c596bd738f5949c60f610c8c906fcce
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-15 22:40:48 +1300

    Implemented generate and assemble for Sinusoid

--- cpp/Examples/Sinusoid.cpp
+++ cpp/Examples/Sinusoid.cpp
@@ -1,4 +1,5 @@
 #include "Sinusoid.h"
+#include <cmath>
 
 namespace InfoNest
 {
@@ -10,8 +11,29 @@ Sinusoid::Sinusoid()
 
 }
 
+void Sinusoid::generate(RNG& rng)
+{
+    A = -log(1.0 - rng.rand());
+    T = exp(log(1E-3 * t_range) + log(1E3) * rng.rand());
+    phi = 2.0 * M_PI * rng.rand();
+
+    for(double& nn : n)
+        nn = rng.randn();
+
+    assemble();
+}
 
 
+void Sinusoid::assemble()
+{
+    double t;
+    double omega = 2*M_PI/T; // Angular frequency
+    for(size_t i=0; i<N; ++i)
+    {
+        t = t_min + i * dt;
+        y[i] = A * sin(omega*t + phi);
+    }
+}
 
 
 
