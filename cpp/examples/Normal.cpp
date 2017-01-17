29beb7b29317d8492d9b7c0965e5be4897937cce
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-18 10:14:30 +1300

    Gutted Normal class, preparing to make it slightly more complex

--- cpp/examples/Normal.cpp
+++ cpp/examples/Normal.cpp
@@ -4,6 +4,7 @@ namespace InfoNest
 {
 
 Normal::Normal()
+:xs(100)
 {
 
 }
@@ -11,46 +12,46 @@ Normal::Normal()
 
 void Normal::generate(RNG& rng)
 {
-    n1 = rng.randn();
-    n2 = rng.randn();
-    assemble();
+    mu = 10.0 * rng.randn();
+    for(double& x: xs)
+        x = mu + rng.randn();
 }
 
 double Normal::perturb(RNG& rng)
 {
     double logH = 0.0;
 
-    // Choose a coordinate to perturb
-    int which = rng.rand_int(2);
-    double& n = (which == 0)?(n1):(n2);
+    int proposal_type = rng.rand_int(4);
 
-    logH -= -0.5*n*n;
-    n += rng.randh();
-    logH += -0.5*n*n;
+    if(proposal_type == 0)
+    {
 
-    assemble();
+    }
+    else if(proposal_type == 1)
+    {
+
+    }
+    else if(proposal_type == 2)
+    {
+
+    }
+    else
+    {
+
+    }
+
 
     return logH;
 }
 
-void Normal::assemble()
-{
-    x = 10 * n1;
-    y = x + n2;
-}
-
 void Normal::print(std::ostream& out)
 {
-    out<<x<<' '<<y<<' ';
+    out<<mu<<' ';
 }
 
 double Normal::distance(const Normal& normal1, const Normal& normal2)
 {
-    // Currently set up for H[p(x, y)]
-    double d1 = std::abs(normal1.x - normal2.x);
-    double d2 = std::abs(normal1.y - normal2.y);
-
-    return (d2 > d1)?(d2):(d1);
+    return 0.0;
 }
 
 } // namespace InfoNest
--- cpp/examples/Normal.h
+++ cpp/examples/Normal.h
@@ -10,39 +10,16 @@ namespace InfoNest
 
 /*
 * A 2D gaussian example.
-* p(x)     ~ N(0, 10^2)
-* p(y | x) ~ N(x, 1)
-* ==>
-* p(y)     ~ N(0, sd=sqrt(101))
-
-* H[p(x)]    = 3.72152363.            (differential entropy)
-* H[p(y)]    = 3.72649879.            (differential entropy)
-* H[p(x, y)] = 5.14046216.            (differential entropy)
-* I[x; y]    = 2.3075603.             (mutual information)
-*/
-
-/*
-* Numerical results:
-* NB: these algorithm computes log-probabilities of meeting the tolerance,
-*     so a correction factor of log(2E-3) (log-volume)
-*     has been applied to make them match the above by converting probability
-*     to density. For H[p(x,y)], the correction factor is 2*log(2E-3).
-*  H[p(x)]   = 3.761 +- 0.040
-*  H[p(y)]   = 3.776 +- 0.048
-*  H[p(x,y)] = 5.167 +- 0.055
-*  I[x; y]   = 2.370 +- 0.083 (computed from the above).
+* p(mu)     ~ N(0, 10^2)
+* p(x | mu) ~ N(x, 1),    and there are 100 of them
 */
 
 class Normal
 {
     private:
         // The two actual quantities
-        double x;
-        double y;
-
-        // Latent coordinates
-        double n1, n2;
-        void assemble();
+        double mu;
+        std::vector<double> xs;
 
     public:
         // Do-nothing constructor
@@ -58,7 +35,8 @@ class Normal
         void print(std::ostream& out);
 
         // Distance function
-        static double distance(const Normal& normal1, const Normal& normal2);
+        static double distance(const Normal& normal1,
+                               const Normal& normal2);
 };
 
 } // namespace InfoNest
