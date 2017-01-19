4ba6c798da3933c867ea2c64a25515f099483758
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-20 12:08:01 +1300

    Greater control in main

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,18 +3,27 @@
 #include <ctime>
 #include "Execute.hpp"
 #include "RNG.h"
-#include "examples/Normal.h"
+#include "examples/AllExamples.h"
 
 // MAIN
 int main()
 {
+    // What example are we using?
+    typedef InfoNest::Normal TheExample;
+
     // Create random number generator, seeded with time
     InfoNest::RNG rng(time(0));
 
-    // Do the runs.
-    InfoNest::execute<InfoNest::Normal>(rng, 30.0, 1000, 1, 1000);
+    // Define run parameters
+    constexpr double depth         = 30.0;
+    constexpr size_t num_reps      = 1000;
+    constexpr size_t num_particles = 1;
+    constexpr size_t mcmc_steps    = 1000;
+
+    // Do the run.
+    InfoNest::execute<TheExample>(rng, depth, num_reps, num_particles,
+                                  mcmc_steps);
 
     return 0;
 }
-
 
