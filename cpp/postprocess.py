c469d8030232eef718bc43b9cc54198521217135
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-01-24 14:58:13 +1300

    Rewrite so that entire contents of output.txt is not loaded into RAM

--- cpp/postprocess.py
+++ cpp/postprocess.py
@@ -1,107 +1,66 @@
-__all__ = ["postprocess", "my_loadtxt"]
+__all__ = ["postprocess"]
 
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-def postprocess(num_particles=1, tol=1E-3):
+def postprocess(tol=1E-3):
     """
     Load the output file and estimate the entropy.
     """
 
-    # Load output file
-    output = my_loadtxt("output.txt")
+    # Load run info
     num_particles = np.loadtxt("run_data.txt")
 
-    # Find the starts of the runs
-    start_indices = np.nonzero(output[:,0] == 1)[0]
+    # Open output.txt
+    f = open("output.txt", "r")
 
-    reps = len(start_indices)
+    # Store results in these
     counts = []
-    last_run_size = None
-    warn = False
+    rep_lengths = []
 
-    for i in range(0, reps):
-        start_index = start_indices[i]
-        if i < reps - 1:
-            run = output[start_index : start_indices[i+1], :]
-        else:
-            run = output[start_index : , :]
+    while True:
+        # Read values until end of file
+        line = f.readline()[0:-1]
+        values = line.split(" ")
+        final_line = len(values) != 2
 
-        # Don't count the last rep if it's incomplete
-        if last_run_size != None and run.shape[0] != last_run_size:
+        # The values on the line
+        if not final_line:
+            k, v = int(values[0]), float(values[1])
+
+        # Start a count at zero
+        if k == 1:
+            counts.append(0)
+            rep_lengths.append(0)
+
+        # Accumulate results
+        if not final_line:
+            if v >= tol:
+                counts[-1] += 1
+            rep_lengths[-1] += 1
+
+        if final_line:
             break
-        last_run_size = run.shape[0]
-
-        count = (run[:,1] > tol).sum()
-        counts.append((run[:,1] > tol).sum())
-        if count == run.shape[0]:
-            warn = True
-
-        print("Processed run {k}/{N}.".format(k=i+1, N=reps))
 
     # Convert to numpy array
     counts = np.array(counts)
+    rep_lengths = np.array(rep_lengths)
 
-    if len(counts) < reps:
-        print("Skipping run {reps} as it is incomplete.".format(reps=reps))
+    if len(rep_lengths) > 1 and rep_lengths[-1] != rep_lengths[0]:
+        counts = counts[0:-1]
+        rep_lengths = rep_lengths[0:-1]
+        print("Skipping last rep as it is incomplete.")
 
+    print("Found {a} complete reps.".format(a=len(counts)))
     print("mean(depth) = {mc}.".format(mc=counts.mean() / num_particles))
     print("std (depth) = {sd}.".format(sd=counts.std()  / num_particles))
     print("sem (depth) = {sem}.".format(\
             sem=counts.std() / num_particles/np.sqrt(len(counts))))
 
-    if warn:
+    if np.any(counts == rep_lengths):
         print("WARNING: A run didn't achieve the desired tolerance.")
-
-#    # Estimate of the differential entropy
-#    # log(probability) ~= log(2 * tol * density)
-#    H = counts.mean() / num_particles + np.log(2 * tol)
-#    stderr = (counts.std() / num_particles) / np.sqrt(reps)
-#    print("H = {H} +- {stderr} nats.".format(H=H, stderr=stderr))
-#    plt.hist(mean_depth, 100, color=[0.2, 0.2, 0.2])
-#    plt.show()
-
-
-def my_loadtxt(filename, single_precision=False):
-    """
-    Load rectangular arrays from a file, faster than numpy.loadtxt does it.
-    """
-    # Open the file
-    f = open(filename, "r")
-
-    # Storage
-    results = []
-
-    # Rows and columns
-    nrow = 0
-    ncol = None
-
-    while(True):
-        # Read the line and split by whitespace
-        line = f.readline()
-        cells = line.split()
-
-        # Quit when you see a different number of columns
-        if ncol is not None and len(cells) != ncol:
-            break
-
-        # Non-comment lines
-        if cells[0][0] != "#":
-            # If it's the first one, get the number of columns
-            if ncol is None:
-                ncol = len(cells)
-
-            # Otherwise, include in results
-            if single_precision:
-                results.append(np.array([float(cell) for cell in cells],\
-                                                          dtype="float32"))
-            else:
-                results.append(np.array([float(cell) for cell in cells]))
-            nrow += 1
-
-    results = np.vstack(results)
-    return results
 
 
 if __name__ == "__main__":
