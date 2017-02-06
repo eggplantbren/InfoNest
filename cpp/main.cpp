5d9895b47a685097a805eb7ae4975761e9f670d5
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-02-06 14:51:57 +1300

    Probably a sensible default

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -21,8 +21,8 @@ int main()
     // Define run parameters
     constexpr double depth         = 1200.0;
     constexpr size_t num_reps      = 1000;
-    constexpr size_t num_particles = 1;
-    constexpr size_t mcmc_steps    = 1000;
+    constexpr size_t num_particles = 10;
+    constexpr size_t mcmc_steps    = 10000;
 
     // Do the run.
     InfoNest::execute<TheExample>(rng1, rng2, depth, num_reps, num_particles,
