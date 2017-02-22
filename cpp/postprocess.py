1a38e09ebf030aa337307546f4e0833add74775c
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-02-22 18:36:35 +1300

    tol=1E-2

--- cpp/postprocess.py
+++ cpp/postprocess.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-def postprocess(tol=1E-3):
+def postprocess(tol=1E-2):
     """
     Load the output file and estimate the entropy.
     """
