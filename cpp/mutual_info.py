683ce998ec059cac1f543aae85e4960231ee45a6
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-02-23 13:11:32 +1300

    Moved .py file

--- cpp/mutual_info.py
+++ cpp/mutual_info.py
@@ -4,14 +4,14 @@ import numpy as np
 
 # Name of the problem
 name = "groupdiff"
-results1 = np.loadtxt(name + "1.txt")
-results2 = np.loadtxt(name + "2.txt")
-results3 = np.loadtxt(name + "3.txt")
+results1 = np.loadtxt("results/" + name + "1.txt")
+results2 = np.loadtxt("results/" + name + "2.txt")
+results3 = np.loadtxt("results/" + name + "3.txt")
 
 L = min([len(results1), len(results2), len(results3)])
 I = results1[0:L] + results2[0:L] - results3[0:L]
 
 plt.plot(I)
-print("I = " + str(I.mean()) + " +- " + str(I.std()/np.sqrt(L)))
+print("I = " + str(I.mean()) + " +- " + str(I.std()/np.sqrt(L)) + " nats.")
 plt.show()
 
