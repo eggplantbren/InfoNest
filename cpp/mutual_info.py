7f8483aaa0f65214da33d4311b48ae3f70309472
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-02-25 12:26:27 +1300

    Take log volumes into account properly

--- cpp/mutual_info.py
+++ cpp/mutual_info.py
@@ -1,6 +1,11 @@
 # A little script for mutual information calculations
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.special import gammaln
+
+# log-volume of n-ball
+def log_volume(n, e):
+    return 0.5*n*np.log(np.pi) - gammaln(0.5*n+1) + n*np.log(e)
 
 # Name of the problem
 name = "groupdiff"
@@ -8,8 +13,20 @@ results1 = np.loadtxt("results/" + name + "1.txt")
 results2 = np.loadtxt("results/" + name + "2.txt")
 results3 = np.loadtxt("results/" + name + "3.txt")
 
+# Parameter and data dimensions
+dim = [1, 200]
+dim.append(sum(dim))
+
+# Tolerances used
+tol = [0.001, 0.3, 0.3]
+
+# log-volumes
+lvs = [ log_volume(dim[i], tol[i]) for i in range(0, 3) ]
+
 L = min([len(results1), len(results2), len(results3)])
-I = results1[0:L] + results2[0:L] - results3[0:L]
+I =   (results1[0:L] + lvs[0]) \
+    + (results2[0:L] + lvs[1]) \
+    - (results3[0:L] + lvs[2])
 
 plt.plot(I)
 plt.axhline(0.0, linestyle="--", alpha=0.3)
