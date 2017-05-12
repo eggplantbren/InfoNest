228334d34c8def0c7a901dde8f783cd1ce5ca715
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-05-12 17:54:51 +1200

    more nicety

--- paper/prec2.py
+++ paper/prec2.py
@@ -2,22 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-20, 20, 200001)
+x = np.linspace(-10, 10, 50001)
 dx = x[1] - x[0]
 p = np.exp(-0.5*x**2) / np.sqrt(2*np.pi)
+p[np.abs(x) > 5] = 0
 L = 1.0
 
 H = 0.0
 for i in range(0, len(x)):
     left = (x >= x[i] - L) & (x <= x[i])
     P = np.sum(p[left])*dx
-    H += -p[i]*dx*(1.0 + np.log(P))
+    if p[i] > 0:
+        H += -p[i]*dx*(1 + np.log(P))
     if (i+1) % 1000 == 0:
         print(i+1, H)
 
-# grid pts  H
-# 20001     0.53386
-# 30001     0.53453
-# 50001     0.53507
-# 100001    0.53547
-# 200001    0.53567
