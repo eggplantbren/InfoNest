4cc59392ff05e701c3a32bc6a93809143d252010
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-05-15 13:50:55 +1200

    Asymmetric density

--- paper/prec2.py
+++ paper/prec2.py
@@ -2,16 +2,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-10, 10, 50001)
+x = np.linspace(-10, 10, 200001)
 dx = x[1] - x[0]
-p = np.exp(-0.5*x**2) / np.sqrt(2*np.pi)
+p = x + 5.0
 p[np.abs(x) > 5] = 0
 L = 1.0
+p /= p.sum()*dx
+plt.plot(x, p)
+plt.show()
 
 H = 0.0
 for i in range(0, len(x)):
-    left = (x >= x[i] - L) & (x <= x[i])
+    left = (x >= x[i]) & (x <= x[i] + L)
     P = np.sum(p[left])*dx
+
+    # Alternative version uses
+    # right = (x >= x[i]) & (x <= x[i] + L)
+    # Instead of left
+
     if p[i] > 0:
         H += -p[i]*dx*(1 + np.log(P))
     if (i+1) % 1000 == 0:
