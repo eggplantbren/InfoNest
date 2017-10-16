a78c44e1a90138530b68de7bf825d12b5690ac1b
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-10-16 14:49:33 +1300

    Committing before going home

--- talk/figures.py
+++ talk/figures.py
@@ -18,8 +18,39 @@ plt.gca().set_yticks([0.0, 0.1, 0.2, 0.3, 0.4])
 plt.ylim([0.0, 0.4])
 plt.xlabel("$x$")
 plt.ylabel("Probability")
-plt.title("$H \\approx 1.0986$ nats")
+plt.title("$H = 1.0986$ nats")
 plt.savefig("entropy1.pdf", bbox_inches="tight")
 print("Created entropy1.pdf")
+
+# Set up a discrete probability distribution
+x = np.arange(5, 26)
+p = np.ones(len(x)) / len(x)
+
+# Make the plot
+plt.figure(figsize=(8, 6.5))
+plt.bar(x, p, color="lightgray", width=0.6)
+plt.ylim([0.0, 0.055])
+plt.xlabel("$x$")
+plt.ylabel("Probability")
+plt.title("$H = 3.0445$ nats")
+plt.savefig("entropy2.pdf", bbox_inches="tight")
+print("Created entropy2.pdf")
+
+
+# Set up a discrete probability distribution
+x = np.hstack([np.arange(5, 21), np.arange(41, 46)])
+p = np.ones(len(x)) / len(x)
+
+# Make the plot
+plt.figure(figsize=(8, 6.5))
+plt.bar(x, p, color="lightgray", width=0.6)
+plt.ylim([0.0, 0.055])
+plt.xlabel("$x$")
+plt.ylabel("Probability")
+plt.title("$H = 3.0445$ nats")
+plt.savefig("entropy3.pdf", bbox_inches="tight")
+print("Created entropy3.pdf")
+
+
 
 
