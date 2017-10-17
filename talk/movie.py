6e1cbad25116351adb38956d2202671b9631442a
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-10-17 16:57:10 +1300

    Multiple reference points

--- talk/movie.py
+++ talk/movie.py
@@ -40,64 +40,70 @@ y = y[::-1]
 p = density(x, y)
 p /= p.sum()
 
-
-# First image
-plt.figure(figsize=(8, 7))
-plt.imshow(p, extent=[x.min(), x.max(), y.min(), y.max()],
-           cmap="Blues")
-plt.xlabel(r"$x$")
-plt.ylabel(r"$y$")
-plt.title("A joint pdf $f(x, y)$")
-plt.savefig("movie/image1.png", bbox_inches="tight")
-print("Saved movie/image1.png")
+k = 1
 
 
-# Second image
-point = generate()
-xref, yref = point["x"], point["y"]
-plt.plot(xref, yref, "o", color="r", alpha=0.5, markersize=12)
-plt.title("A reference point drawn from $f(x,y)$")
-plt.savefig("movie/image2.png", bbox_inches="tight")
-print("Saved movie/image2.png")
+for rep in range(10):
 
+    # First image
+    plt.figure(figsize=(8, 7))
+    plt.imshow(p, extent=[x.min(), x.max(), y.min(), y.max()],
+               cmap="Blues")
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$y$")
+    plt.title("A joint pdf $f(x, y)$")
+    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
+    print("Saved movie/image{k}.png".format(k=k))
+    k += 1
 
-# Add contours of the distance function
-dist = np.sqrt((x - xref)**2 + (y - yref)**2)
-plt.contour(x, y, dist, 50, alpha=0.2, colors="red", linestyles="--")
-plt.title("Distance from the reference point")
-plt.savefig("movie/image3.png", bbox_inches="tight")
-print("Saved movie/image3.png")
+    # Second image
+    point = generate()
+    xref, yref = point["x"], point["y"]
+    plt.plot(xref, yref, "o", color="r", alpha=0.5, markersize=12)
+    plt.title("A reference point drawn from $f(x,y)$")
+    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
+    print("Saved movie/image{k}.png".format(k=k))
+    k += 1
 
+    # Add contours of the distance function
+    dist = np.sqrt((x - xref)**2 + (y - yref)**2)
+    plt.contour(x, y, dist, 50, alpha=0.2, colors="red", linestyles="--")
+    plt.title("Distance from the reference point")
+    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
+    print("Saved movie/image{k}.png".format(k=k))
+    k += 1
 
-# Add Nested Sampling points
-ns_points = generate(10)
-plt.plot(ns_points["x"], ns_points["y"], "*", color="green",
-         markersize=10, alpha=0.8)
-plt.title("Nested Sampling")
-plt.savefig("movie/image4.png", bbox_inches="tight")
-print("Saved movie/image4.png")
-
-# NS loop
-for i in range(0, 60):
-
-    # Find worst point
-    dist = np.sqrt((ns_points["x"] - xref)**2 + (ns_points["y"] - yref)**2)
-    worst = np.nonzero(dist == np.max(dist))[0]
-    plt.plot(ns_points["x"][worst], ns_points["y"][worst], "^", color="black",
+    # Add Nested Sampling points
+    ns_points = generate(10)
+    plt.plot(ns_points["x"], ns_points["y"], "*", color="green",
              markersize=10, alpha=0.8)
-    plt.savefig("movie/image{k}.png".format(k=i+5), bbox_inches="tight")
-    print("Saved movie/image{k}.png".format(k=i+5))
+    plt.title("Nested Sampling")
+    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
+    print("Saved movie/image{k}.png".format(k=k))
+    k += 1
 
-    # Replace worst point
-    threshold = dist[worst]
-    while True:
-        new = generate()
-        dist_new = np.sqrt((new["x"] - xref)**2 + (new["y"] - yref)**2)
-        if dist_new < threshold:
-            ns_points["x"][worst] = new["x"]
-            ns_points["y"][worst] = new["y"]
+    # NS loop
+    for i in range(0, 60):
 
-            plt.plot(new["x"], new["y"], "*", color="green",
-                        markersize=10, alpha=0.8)
-            break
+        # Find worst point
+        dist = np.sqrt((ns_points["x"] - xref)**2 + (ns_points["y"] - yref)**2)
+        worst = np.nonzero(dist == np.max(dist))[0]
+        plt.plot(ns_points["x"][worst], ns_points["y"][worst], "^", color="black",
+                 markersize=10, alpha=0.8)
+        plt.savefig("movie/image{k}.png".format(k=i+5), bbox_inches="tight")
+        print("Saved movie/image{k}.png".format(k=i+5))
+
+        # Replace worst point
+        threshold = dist[worst]
+        while True:
+            new = generate()
+            dist_new = np.sqrt((new["x"] - xref)**2 + (new["y"] - yref)**2)
+            if dist_new < threshold:
+                ns_points["x"][worst] = new["x"]
+                ns_points["y"][worst] = new["y"]
+
+                plt.plot(new["x"], new["y"], "*", color="green",
+                            markersize=10, alpha=0.8)
+                k += 1
+                break
 
