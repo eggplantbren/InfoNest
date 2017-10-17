75dfbae30019d7fa9df25a1367a549728ef54877
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-10-17 19:18:25 +1300

    A bunch of editing

--- talk/movie.py
+++ talk/movie.py
@@ -26,9 +26,20 @@ def generate(num=1):
     """
     Generate from the above density.
     """
-    x = 3*rng.randn(num)
-    y = 0.1*x*np.abs(x) + 2*rng.randn(num)
-    return {"x": x, "y": y}
+
+    xx = []
+    yy = []
+
+    for i in range(num):
+        while True:
+            x = 3*rng.randn()
+            y = 0.1*x*np.abs(x) + 2*rng.randn()
+            if np.abs(x) < 10.0 and np.abs(y) < 10.0:
+                break
+        xx.append(x)
+        yy.append(y)
+
+    return {"x": np.array(xx), "y": np.array(yy)}
 
 
 # Set up a grid
@@ -48,51 +59,59 @@ for rep in range(10):
     # First image
     plt.clf()
     plt.figure(figsize=(8, 7))
-    plt.imshow(p, extent=[x.min(), x.max(), y.min(), y.max()],
+    plt.imshow(np.sqrt(p), extent=[x.min(), x.max(), y.min(), y.max()],
                cmap="Blues")
     plt.xlabel(r"$x$")
     plt.ylabel(r"$y$")
     plt.title("A joint pdf $f(x, y)$")
-    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
-    print("Saved movie/image{k}.png".format(k=k))
-    k += 1
+
+    for j in range(20):
+        plt.savefig("movie/image{k}.png".format(k="%0.6d" % k))
+        print("Saved movie/image{k}.png".format(k="%0.6d" % k))
+        k += 1
 
     # Second image
     point = generate()
     xref, yref = point["x"], point["y"]
     plt.plot(xref, yref, "o", color="r", alpha=0.5, markersize=12)
     plt.title("A reference point drawn from $f(x,y)$")
-    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
-    print("Saved movie/image{k}.png".format(k=k))
-    k += 1
+
+    for j in range(20):
+        plt.savefig("movie/image{k}.png".format(k="%0.6d" % k))
+        print("Saved movie/image{k}.png".format(k="%0.6d" % k))
+        k += 1
 
     # Add contours of the distance function
     dist = np.sqrt((x - xref)**2 + (y - yref)**2)
-    plt.contour(x, y, dist, 50, alpha=0.2, colors="red", linestyles="--")
+    plt.contour(x, y, dist, levels=np.linspace(0.0, 40.0, 51),
+                alpha=0.2, colors="red", linestyles="--")
     plt.title("Distance from the reference point")
-    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
-    print("Saved movie/image{k}.png".format(k=k))
-    k += 1
+
+    for j in range(20):
+        plt.savefig("movie/image{k}.png".format(k="%0.6d" % k))
+        print("Saved movie/image{k}.png".format(k="%0.6d" % k))
+        k += 1
 
     # Add Nested Sampling points
     ns_points = generate(10)
     plt.plot(ns_points["x"], ns_points["y"], "*", color="green",
              markersize=10, alpha=0.8)
     plt.title("Nested Sampling")
-    plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
-    print("Saved movie/image{k}.png".format(k=k))
-    k += 1
+    for j in range(20):
+        plt.savefig("movie/image{k}.png".format(k="%0.6d" % k))
+        print("Saved movie/image{k}.png".format(k="%0.6d" % k))
+        k += 1
 
     # NS loop
-    for i in range(0, 60):
+    for i in range(0, 50):
 
         # Find worst point
         dist = np.sqrt((ns_points["x"] - xref)**2 + (ns_points["y"] - yref)**2)
         worst = np.nonzero(dist == np.max(dist))[0]
         plt.plot(ns_points["x"][worst], ns_points["y"][worst], "^", color="black",
                  markersize=10, alpha=0.8)
-        plt.savefig("movie/image{k}.png".format(k=k), bbox_inches="tight")
-        print("Saved movie/image{k}.png".format(k=k))
+        plt.savefig("movie/image{k}.png".format(k="%0.6d" % k))
+        print("Saved movie/image{k}.png".format(k="%0.6d" % k))
 
         # Replace worst point
         threshold = dist[worst]
@@ -107,4 +126,5 @@ for rep in range(10):
                             markersize=10, alpha=0.8)
                 k += 1
                 break
+
 
