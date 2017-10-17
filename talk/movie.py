cb6ad451a1356389f6ac5ffca9b4ccf8d112b51a
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-10-17 16:52:01 +1300

    Movie :)

--- talk/movie.py
+++ talk/movie.py
@@ -73,7 +73,31 @@ print("Saved movie/image3.png")
 ns_points = generate(10)
 plt.plot(ns_points["x"], ns_points["y"], "*", color="green",
          markersize=10, alpha=0.8)
-plt.title("Nested Sampling live points")
+plt.title("Nested Sampling")
 plt.savefig("movie/image4.png", bbox_inches="tight")
 print("Saved movie/image4.png")
+
+# NS loop
+for i in range(0, 60):
+
+    # Find worst point
+    dist = np.sqrt((ns_points["x"] - xref)**2 + (ns_points["y"] - yref)**2)
+    worst = np.nonzero(dist == np.max(dist))[0]
+    plt.plot(ns_points["x"][worst], ns_points["y"][worst], "^", color="black",
+             markersize=10, alpha=0.8)
+    plt.savefig("movie/image{k}.png".format(k=i+5), bbox_inches="tight")
+    print("Saved movie/image{k}.png".format(k=i+5))
+
+    # Replace worst point
+    threshold = dist[worst]
+    while True:
+        new = generate()
+        dist_new = np.sqrt((new["x"] - xref)**2 + (new["y"] - yref)**2)
+        if dist_new < threshold:
+            ns_points["x"][worst] = new["x"]
+            ns_points["y"][worst] = new["y"]
+
+            plt.plot(new["x"], new["y"], "*", color="green",
+                        markersize=10, alpha=0.8)
+            break
 
